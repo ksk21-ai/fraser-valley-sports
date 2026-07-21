ba58966fb10555d04bd52684ee4dc0cca55db8e6
unknown <kanavskapur@gmail.com>
2026-07-20 22:26:40 -0700

    Add Firebase Analytics tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,363 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 import 'firebase_options.dart';
-
 import 'screens/home_screen.dart';
 import 'screens/favorites_screen.dart';
 
-
-
-void main() async {
-
-
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-
-  await Firebase.initializeApp(
-
-    options:
-    DefaultFirebaseOptions.currentPlatform,
-
+  await FirebaseAnalytics.instance.logEvent(
+    name: 'app_opened',
+    parameters: {'app_name': 'fraser_valley_sports'},
   );
 
-
-
-  runApp(
-
-    const MyApp(),
-
-  );
-
-
+  runApp(const MyApp());
 }
-
-
-
-
-
-
 
 class MyApp extends StatelessWidget {
-
-
   const MyApp({super.key});
 
+  static final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
+  static final FirebaseAnalyticsObserver analyticsObserver =
+      FirebaseAnalyticsObserver(analytics: analytics);
 
   @override
-  Widget build(BuildContext context){
-
-
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fraser Valley Sports',
 
+      navigatorObservers: [analyticsObserver],
 
-
-      debugShowCheckedModeBanner:false,
-
-
-
-      title:
-      "Fraser Valley Sports",
-
-
-
-
-      theme:
-
-      ThemeData(
-
-
-
-        useMaterial3:true,
-
-
-
-        colorScheme:
-
-        ColorScheme.fromSeed(
-
-          seedColor:
-          Colors.orange,
-
-        ),
-
-
-
-        scaffoldBackgroundColor:
-
-        const Color(0xffF7F7F7),
-
-
-
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        scaffoldBackgroundColor: const Color(0xffF7F7F7),
       ),
 
-
-
-
-      home:
-
-      const MainNavigation(),
-
-
-
+      home: const MainNavigation(),
     );
-
-
   }
-
-
 }
-
-
-
-
-
-
-
-
 
 class MainNavigation extends StatefulWidget {
-
-
   const MainNavigation({super.key});
 
-
-
   @override
-  State<MainNavigation> createState() =>
-      _MainNavigationState();
-
-
-
+  State<MainNavigation> createState() => _MainNavigationState();
 }
 
-
-
-
-
-
-
-
-class _MainNavigationState
-    extends State<MainNavigation>{
-
-
-
+class _MainNavigationState extends State<MainNavigation> {
   int currentIndex = 0;
 
-
-
   final GlobalKey<FavoritesScreenState> favoritesKey =
-  GlobalKey<FavoritesScreenState>();
+      GlobalKey<FavoritesScreenState>();
 
+  Future<void> changeTab(int index) async {
+    setState(() {
+      currentIndex = index;
+    });
 
+    final String screenName = index == 0 ? 'home_screen' : 'favorites_screen';
 
-
-
-
-
-  @override
-  Widget build(BuildContext context){
-
-
-
-    return Scaffold(
-
-
-
-
-      body:
-
-
-      IndexedStack(
-
-
-
-        index:
-
-        currentIndex,
-
-
-
-        children:[
-
-
-
-          const HomeScreen(),
-
-
-
-
-          FavoritesScreen(
-
-            key:
-
-            favoritesKey,
-
-          ),
-
-
-
-        ],
-
-
-
-      ),
-
-
-
-
-
-
-
-      bottomNavigationBar:
-
-
-      NavigationBar(
-
-
-
-        selectedIndex:
-
-        currentIndex,
-
-
-
-
-        onDestinationSelected:
-
-            (index){
-
-
-
-          setState((){
-
-
-
-            currentIndex = index;
-
-
-
-          });
-
-
-
-
-
-
-          // Refresh favorites every time tab opens
-
-          if(index == 1){
-
-
-
-            favoritesKey.currentState
-                ?.loadFavorites();
-
-
-
-          }
-
-
-
-        },
-
-
-
-
-
-        destinations:[
-
-
-
-          const NavigationDestination(
-
-
-
-            icon:
-
-            Icon(
-
-              Icons.home_outlined,
-
-            ),
-
-
-
-            selectedIcon:
-
-            Icon(
-
-              Icons.home,
-
-            ),
-
-
-
-            label:
-
-            "Home",
-
-
-
-          ),
-
-
-
-
-
-          const NavigationDestination(
-
-
-
-            icon:
-
-            Icon(
-
-              Icons.favorite_outline,
-
-            ),
-
-
-
-            selectedIcon:
-
-            Icon(
-
-              Icons.favorite,
-
-            ),
-
-
-
-            label:
-
-            "Favorites",
-
-
-
-          ),
-
-
-
-
-        ],
-
-
-
-      ),
-
-
-
-
+    await FirebaseAnalytics.instance.logScreenView(
+      screenName: screenName,
+      screenClass: 'MainNavigation',
     );
 
+    await FirebaseAnalytics.instance.logEvent(
+      name: 'navigation_tab_selected',
+      parameters: {'tab_name': index == 0 ? 'home' : 'favorites'},
+    );
 
-
+    if (index == 1) {
+      favoritesKey.currentState?.loadFavorites();
+    }
   }
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: [
+          const HomeScreen(),
+          FavoritesScreen(key: favoritesKey),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: changeTab,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.favorite_outline),
+            selectedIcon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
 }
